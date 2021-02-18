780d628579cb762c50866bd44bbde9ca84ddf0e6
Carlos <cjrfls@gmail.com>
2021-02-18 11:30:08 -0700

    Created basic files and folders

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Piedra_papel_o_tijeras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
