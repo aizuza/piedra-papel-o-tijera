aea5f3960a72159711daee12140c96742bd0333f
Carlos <cjrfls@gmail.com>
2021-02-25 10:59:44 -0700

    Se implemento la forma de que la computadora dijera quien ganó

--- Program.cs
+++ Program.cs
@@ -23,6 +23,8 @@ namespace Piedra_papel_o_tijeras
         }
         static void Main(string[] args)
         {
+            //Seleccionar
+            //piedra > tijera > papel > piedra
             int option = SelectOption();
             Console.WriteLine("Opcion seleccionada :" + option);
             int aiOption = SelectRandom();
@@ -31,9 +33,8 @@ namespace Piedra_papel_o_tijeras
             //Seleccionar opción a jugar (piedra, papel o tijera)
             //La computadora genera su opción a jugar de manera aleatoria (piedra, papel o tijera)
             //Determinar quién ganó de acuerdo a las opciones seleccionadas, y mostrar al ganador
-            //Jugar 3 partidas, y mencionar quién ganó al final (jugador o computadora)
-       
-            
+            // [Canceled] Jugar 3 partidas, y mencionar quién ganó al final (jugador o computadora)
+            CheckWinner(option, aiOption);
             
         }
         /// <summary>
@@ -49,5 +50,55 @@ namespace Piedra_papel_o_tijeras
             
             return option;
         }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="option"> elección del jugador</param>
+        /// <param name="aiOption"> elección de la A.I</param>
+
+        static void CheckWinner(int option, int aiOption){
+
+           // string x = "tijera";
+           // string y = "papel";
+           // string z = "piedra";
+            
+            if (option == 1 & aiOption == 2 ) // Piedra vs Tijera 
+            {
+            Console.WriteLine("jugador : piedra");
+            Console.WriteLine("A.I : papel");
+
+            Console.WriteLine("player has won");
+
+        }else if(option == 2 & aiOption == 3) // Tijera vs papel
+        {
+            Console.WriteLine("jugador : papel");
+            Console.WriteLine("A.I : tijera");
+
+            Console.WriteLine("player has won");
+
+        }else if(option == 3 & aiOption == 1) // Papel vs Piedra
+        
+        {
+            Console.WriteLine("jugador : papel");
+            Console.WriteLine("A.I : piedra");
+
+            Console.WriteLine("player has won");
+        }else if(option == aiOption) // empate
+        
+        {
+            //if(option=x  aiOption = x)
+
+            Console.WriteLine("Tie");
+
+
+        }else //La consola gana
+        {
+            Console.WriteLine("The A.I has won");
+        }
+        }
+        
     }
 }
